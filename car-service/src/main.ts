962b8cee757f885ccb135a2dacc92af6693854de
Oscar Liang <oscar.liang@austcommunitymedia.com.au>
2024-01-22 13:42:40 +1100

    add docker

--- car-service/src/main.ts
+++ car-service/src/main.ts
@@ -4,6 +4,7 @@ import { Transport, TcpOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const API_GATEWAY_PORT = Number(process.env.PORT);
+  const GRAPHQL_GATEWAY_PORT = Number(process.env.GRAPHQL_PORT);
   const microserviceTcp = await NestFactory.createMicroservice(CarModule, {
     transport: Transport.TCP,
     options: {
@@ -19,11 +20,13 @@ async function bootstrap() {
 
   const app = await NestFactory.create(CarModule);
   app.enableCors();
-  await app.listen(API_GATEWAY_PORT, () => {
+  await app.listen(GRAPHQL_GATEWAY_PORT, () => {
     console.log(
-      `CAR_SERVICE_API ENDPOINT: http://localhost:${API_GATEWAY_PORT}`,
+      `CAR_SERVICE_API ENDPOINT: http://localhost:${GRAPHQL_GATEWAY_PORT}`,
     );
-    console.log(`Swagger ENDPOINT: http://localhost:${API_GATEWAY_PORT}/api`);
+    console.log(
+      `Swagger ENDPOINT: http://localhost:${GRAPHQL_GATEWAY_PORT}/api`,
+    );
   });
 }
 bootstrap();
